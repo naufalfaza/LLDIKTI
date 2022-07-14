21dabb1b397c5a0b4b3d46d7f309929e588fc216
naufalfaza <naufalfaza410502@gmail.com>
2022-07-14 14:26:49 +0700

    LOMBA WEB LLDIKTI 4

---
+++ application/views/structure/foot.php
@@ -0,0 +1,22 @@
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>Copyright © 2021 SEO Dream Co., Ltd. All Rights Reserved. 
+          
+          <br>Web Designed by <a rel="nofollow" href="https://templatemo.com" title="free CSS templates">TemplateMo</a></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Scripts -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/animation.js"></script>
+  <script src="assets/js/imagesloaded.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+</body>
+</html>
